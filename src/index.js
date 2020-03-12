136115d46aaef53889d3c488f87050853f919654
Anton <samusenkoanton@yandex.ru>
2020-03-13 00:53:31 +0300

    feat add: solution
    add solution to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n){
+    n = Math.abs(n);
+    n = String(n).split("").reverse();
+    n = n.reduce(function(sum, current) {
+        return sum + current;
+    },);
+    return Number(n);
 }
